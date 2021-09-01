a9df494ba540c121da50f1da9c51567a54ed869a
VitorSoler <83377382+VitorSoler@users.noreply.github.com>
2021-09-01 16:19:47 -0300

    Add files via upload

---
+++ GeradorDeSenhas/GeradorDeSenhas/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace GeradorDeSenhas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                char Resposta;
+                do
+                {
+                    string Caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToLower() + "1234567890" + "!@#$%¨&*()_+-=§/?°´`''|₢<>,.;:~^{}[]ªº£";
+
+                    Console.Write("Digite a quantidade de caracteres que a sua senha terá: ");
+                    int Quantidade = int.Parse(Console.ReadLine());
+
+                    Random Sorteio = new Random();
+                    int CaracteresSorteados = 0;
+
+                    StringBuilder SenhaFinal = new StringBuilder();
+                    for (int i = 0; i < Quantidade; i++)
+                    {
+                        CaracteresSorteados = Sorteio.Next(0, Caracteres.Length);
+                        SenhaFinal.Append(Caracteres[CaracteresSorteados]);
+                    }
+                    Console.WriteLine(SenhaFinal);
+
+                    Console.WriteLine();
+                    Console.Write("Deseja fazer uma nova senha(s/n)? ");
+                    Resposta = char.Parse(Console.ReadLine());
+                    Console.Clear();
+                }
+                while (Resposta == 's' || Resposta == 'S');
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine(erro.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
